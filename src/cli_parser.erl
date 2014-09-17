42bf244e142867ed15fe8475565f7bd90d4fa48d
Evgeniy (enepomnyashchiy) Nepomniashchiy <rush.zlo@gmail.com>
2014-09-17 11:17:50 +0400

    split cli to parser and handlers

---
+++ src/cli_parser.erl
@@ -0,0 +1,140 @@
+%% @author rush
+%% @doc @todo Add description to cli_parser.
+
+
+-module(cli_parser).
+-behaviour(gen_server).
+
+-export([start_link/0, evaluate/1, expand/1]).
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
+
+-record(state, {commands}).
+
+%% ====================================================================
+%% API functions
+%% ====================================================================
+
+% return {ok, Pid}
+start_link() ->
+  gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
+
+%% evaluate command line
+evaluate(Line) ->
+	gen_server:call(?MODULE, {evaluate, Line}).
+
+%% try to expand command line
+expand(RevBeforeCursor) ->
+	gen_server:call(?MODULE, {expand, lists:reverse(RevBeforeCursor)}).
+
+%% ====================================================================
+%% Behavioural functions 
+%% ====================================================================
+
+%% init/1
+%% ====================================================================
+%% @doc <a href="http://www.erlang.org/doc/man/gen_server.html#Module:init-1">gen_server:init/1</a>
+-spec init(Args :: term()) -> Result when
+	Result :: {ok, State}
+			| {ok, State, Timeout}
+			| {ok, State, hibernate}
+			| {stop, Reason :: term()}
+			| ignore,
+	State :: term(),
+	Timeout :: non_neg_integer() | infinity.
+%% ====================================================================
+init([]) ->
+	% register all available commands from cli modules
+	Commands = register([cli_utils]),
+    {ok, #state{commands=[Commands]}}.
+
+%% handle_call/3
+%% ====================================================================
+%% @doc <a href="http://www.erlang.org/doc/man/gen_server.html#Module:handle_call-3">gen_server:handle_call/3</a>
+-spec handle_call(Request :: term(), From :: {pid(), Tag :: term()}, State :: term()) -> Result when
+	Result :: {reply, Reply, NewState}
+			| {reply, Reply, NewState, Timeout}
+			| {reply, Reply, NewState, hibernate}
+			| {noreply, NewState}
+			| {noreply, NewState, Timeout}
+			| {noreply, NewState, hibernate}
+			| {stop, Reason, Reply, NewState}
+			| {stop, Reason, NewState},
+	Reply :: term(),
+	NewState :: term(),
+	Timeout :: non_neg_integer() | infinity,
+	Reason :: term().
+%% ====================================================================
+handle_call({evaluate, Line}, _From, State) ->
+    {reply, {ok, evaluated, command, string:tokens(Line, " \n")}, State};
+handle_call({expand, _Line}, _From, State) ->
+	{reply, {no, " inserted ", ["proposal1", "proposal2"]}, State};
+handle_call(_Request, _From, State) ->
+    Reply = ok,
+    {reply, Reply, State}.
+
+
+%% handle_cast/2
+%% ====================================================================
+%% @doc <a href="http://www.erlang.org/doc/man/gen_server.html#Module:handle_cast-2">gen_server:handle_cast/2</a>
+-spec handle_cast(Request :: term(), State :: term()) -> Result when
+	Result :: {noreply, NewState}
+			| {noreply, NewState, Timeout}
+			| {noreply, NewState, hibernate}
+			| {stop, Reason :: term(), NewState},
+	NewState :: term(),
+	Timeout :: non_neg_integer() | infinity.
+%% ====================================================================
+handle_cast(_Msg, State) ->
+    {noreply, State}.
+
+
+%% handle_info/2
+%% ====================================================================
+%% @doc <a href="http://www.erlang.org/doc/man/gen_server.html#Module:handle_info-2">gen_server:handle_info/2</a>
+-spec handle_info(Info :: timeout | term(), State :: term()) -> Result when
+	Result :: {noreply, NewState}
+			| {noreply, NewState, Timeout}
+			| {noreply, NewState, hibernate}
+			| {stop, Reason :: term(), NewState},
+	NewState :: term(),
+	Timeout :: non_neg_integer() | infinity.
+%% ====================================================================
+handle_info(_Info, State) ->
+    {noreply, State}.
+
+
+%% terminate/2
+%% ====================================================================
+%% @doc <a href="http://www.erlang.org/doc/man/gen_server.html#Module:terminate-2">gen_server:terminate/2</a>
+-spec terminate(Reason, State :: term()) -> Any :: term() when
+	Reason :: normal
+			| shutdown
+			| {shutdown, term()}
+			| term().
+%% ====================================================================
+terminate(_Reason, _State) ->
+    ok.
+
+
+%% code_change/3
+%% ====================================================================
+
+%% @doc <a href="http://www.erlang.org/doc/man/gen_server.html#Module:code_change-3">gen_server:code_change/3</a>
+-spec code_change(OldVsn, State :: term(), Extra :: term()) -> Result when
+	Result :: {ok, NewState :: term()} | {error, Reason :: term()},
+	OldVsn :: Vsn | {down, Vsn},
+	Vsn :: term().
+%% ====================================================================
+code_change(_OldVsn, State, _Extra) ->
+    {ok, State}.
+
+%% ====================================================================
+%% Internal functions
+%% ====================================================================
+
+% register all available commands from cli modules
+-spec register( Modules :: list(module()) ) -> Result :: list().
+register([]) -> [];
+register([Module | Modules]) ->
+	io:format("* debug: register cli module ~p~n", [Module]),
+	[ apply(Module, register, []) | register(Modules) ].
